5070f786bd101b70b61623111d91a8f662895f1f
tavisca-smurkar <smurkar@tavisca.com>
2016-09-30 08:42:21 +0530

    Extenstion method added and tested

--- ExtensionMethodDemo/Program.cs
+++ ExtensionMethodDemo/Program.cs
@@ -10,6 +10,16 @@ namespace ExtensionMethodDemo
     {
         static void Main(string[] args)
         {
+            string str = "shreekesh";
+            string result = str.ChangeTheCaseOfFirstLetter();
+            Console.WriteLine(result);
+
+            List<int> Numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            IEnumerable<int> EvenNumbers = Enumerable.Where(Numbers, n => n % 2 == 0);
+            foreach (var evenNumber in EvenNumbers)
+            {
+                Console.WriteLine(evenNumber);
+            }
         }
     }
 }
---
+++ ExtensionMethodDemo/StringHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodDemo
+{
+   public static class StringHelper
+    {
+        public static string ChangeTheCaseOfFirstLetter(this string oldString)
+        {
+            if (oldString.Length != 0)
+            {
+                char[] newString = oldString.ToCharArray();
+                newString[0] = char.IsUpper(newString[0]) ? char.ToLower(newString[0]) : char.ToUpper(newString[0]);
+                return new string(newString);
+            }
+
+            
+            return oldString;
+           
+        }
+    }
+}
